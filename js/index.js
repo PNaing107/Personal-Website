1d03221a17b46cb61d5a5cf1ec672e968ef0716a
Phone Naing <phone@huboo.co.uk>
2022-12-22 15:32:01 +0000

    braindumps page mvp done

--- js/index.js
+++ js/index.js
@@ -16,82 +16,82 @@
 // Bootcamp Braindump Post Renderer
 const posts = [
     {
-        id: 1,
+        id: '01',
         title: 'HTML and CSS',
         link: './week1.html'
     },
     {
-        id: 2,
+        id: '02',
         title: 'Building a Portfolio Website',
         link: './week1.html'
     },
     {
-        id: 3,
+        id: '03',
         title: 'Introduction to Programming',
         link: './week1.html'
     },
     {
-        id: 4,
+        id: '04',
         title: 'Programming Best Practices',
         link: './week1.html'
     },
     {
-        id: 5,
+        id: '05',
         title: 'Building a Collection App',
         link: './week1.html'
     },
     {
-        id: 6,
+        id: '06',
         title: 'Introduction to Javascript',
         link: './week1.html'
     },
     {
-        id: 7,
+        id: '07',
         title: 'Building a Game with Javascript',
         link: './week1.html'
     },
     {
-        id: 8,
+        id: '08',
         title: 'Frontend Tools',
         link: './week1.html'
     },
     {
-        id: 9,
+        id: '09',
         title: 'Building a Frontend with React & Bootstrap',
         link: './week1.html'
     },
     {
-        id: 10,
+        id: '10',
         title: 'Advanced Programming Principles',
         link: './week1.html'
     },
     {
-        id: 11,
+        id: '11',
         title: 'Building a Backend with PHP',
         link: './week1.html'
     },
     {
-        id: 12,
+        id: '12',
         title: 'MVC & PHP Frameworks',
         link: './week1.html'
     },
     {
-        id: 13,
+        id: '13',
         title: 'Working with Legacy Code & Green Software Development',
         link: './week1.html'
     },
     {
-        id: 14,
+        id: '14',
         title: 'Node.js',
         link: './week1.html'
     },
     {
-        id: 15,
+        id: '15',
         title: 'Levelling Up React',
         link: './week1.html'
     },
     {
-        id: 16,
+        id: '16',
         title: 'Showcase Project',
         link: './week1.html'
     }
@@ -104,7 +104,7 @@ const link = document.querySelector('a');
 
 select.addEventListener('change', (ev) => {
     let week = ev.target.value;
-    h3.innerText = week;
+    h3.innerText = posts[week - 1]['id'];
     h2.innerText = posts[week - 1]['title'];
     link.setAttribute('href', posts[week - 1]['link']);
 });
