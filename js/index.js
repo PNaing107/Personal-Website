3f95403a745ea095dbb8f45f88a6e3ad29e3a38d
Phone Naing <phone@huboo.co.uk>
2022-12-23 15:49:52 +0000

    removing modal

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-// const button = document.querySelector('.button-34');
-// const modalContainer = document.querySelector('#modal-container');
-// const body=document.querySelector('body');
-
-// button.addEventListener('click', () => {
-//     modalContainer.removeAttribute('class');
-//     modalContainer.classList.add('four');
-//     body.classList.add('modal-active');
-// })
-
-// modalContainer.addEventListener('click', () => {
-//     modalContainer.classList.add('out');
-//     body.classList.remove('modal-active');
-// })
-
 // Bootcamp Braindump Post Renderer
 const posts = [
     {
